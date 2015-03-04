312ec6119597be7cdd44e0d31533dbd64f56e1ef
Nick Malleson <n.malleson06@leeds.ac.uk>
2015-03-04 13:00:07 +0000

    working on programme page

--- programme/csv2json.py
+++ programme/csv2json.py
@@ -4,6 +4,15 @@
 import json
 import pandas as pd
 import re
+import math
+
+def check(s):
+    """Checks that the input string, s, has some content. If not, return 'na'. Note that pandas sets
+    some data to be 'nan', which is actually a number. Hence the need to check for floats"""
+    if s == None or s == "" or type(s) == type(1.0): 
+        return 'na'
+    else:
+        return s
 
 # Read the csv file using pandas handy csv reading function
 data =  pd.read_csv("papers_db.csv")
@@ -14,8 +23,8 @@ for i in range(len(data)):
     d = {}
     d['PaperNo'] = data['PaperNo'][i]
     d['title'] = data['title'][i]
-    d['group'] = data['Group'][i]
-    d['category'] = data['category'][i]
+    d['group'] = check(data['Group'][i])
+    d['category'] = check(data['category'][i])
     # Split authors string on commas and 'and'
     authors = []
     for auth in re.split(r',|and', data['authors'][i].strip()):
