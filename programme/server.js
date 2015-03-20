fddae65cc50511f250858468e0b37b0dc02deb6d
Nick Malleson <n.malleson06@leeds.ac.uk>
2015-03-20 16:08:10 +0000

    Created new branch called "programme" - moving programme stuff to there

--- programme/server.js
+++
@@ -1,4 +0,0 @@
-// First need to install:  npm install connect serve-static
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
